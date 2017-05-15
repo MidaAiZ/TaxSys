50cc57bffd3fad750a3e59d8a92898fb0d317411
Engineer_DDP <zzaddp@live.com>
2017-05-15 16:01:50 +0800

    配置增值税计算接口

---
+++ src/main/java/com/taxsys/taxStrategyImpl/StandardSimpleCalculator.java
@@ -0,0 +1,50 @@
+package com.taxsys.taxStrategyImpl;
+
+import com.taxsys.taxStrategy.ICalculateMethod;
+
+/**
+ * Created by ZZADD on 2017/5/14.
+ */
+public class StandardSimpleCalculator implements ICalculateMethod {
+    /**
+     * 设置主要税率参数
+     *
+     * @param rate 税率值
+     */
+    public void setVATRate(double rate) {
+
+    }
+
+    /**
+     * 设置次要税率参数
+     *
+     * @param rate 税率值
+     * @throws IllegalAccessException
+     */
+    public void setVATRateSecondary(double rate) throws IllegalAccessException {
+
+    }
+
+    /**
+     * 通过净成本值计算增值税额
+     *
+     * @param income  净收入
+     * @param outcome 净支出
+     * @return
+     */
+    public double getVAT(double income, double outcome) {
+        return 0;
+    }
+
+    /**
+     * 通过税后发票计算增值税额
+     *
+     * @param income        销项/含税收入
+     * @param outcome       进项/含税支出
+     * @param isTaxIncluded 是否为含税额
+     * @return
+     */
+    public double getVAT(double income, double outcome, boolean isTaxIncluded) {
+        return 0;
+    }
+}
