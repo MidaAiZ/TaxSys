3dfcb90398f6dcac4e4df3232ca43ab841d631c0
Engineer_DDP <zzaddp@live.com>
2017-06-18 18:43:58 +0800

    构建针对季度预测的多元线性回归预测算法

--- src/main/java/com/taxcal/Analysis/SeasonData.java
+++ src/main/java/com/taxcal/Analysis/SeasonData.java
@@ -2,29 +2,26 @@ package com.taxcal.Analysis;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
- * Created by ZZADD on 2017/6/8.
+ * 二维坐标
  */
 
-public class SeasonData{
+public class SeasonData implements Iterable<Float>{
     private List<Float> data;
     private double sum;
-    private static int SeasonCount = 12;
-
-    public static void SetSeasonCount(int c)
-    {
-        SeasonCount = c;
-    }
 
     public SeasonData(float[] input)
     {
         this.data = new ArrayList<Float>();
         this.sum = 0;
         int i = 0;
-        while(i < SeasonCount)
+        while(i < input.length)
         {
             this.data.add(input[i]);
+            i++;
         }
     }
     public double getTotal(){
@@ -40,4 +37,44 @@ public class SeasonData{
         return this.data.get(index);
     }
     public int getCount(){return this.data.size(); }
+
+    public Iterator<Float> iterator(){
+        return new Iterator<Float>(){
+            int cursor = 0;
+
+            public boolean hasNext(){
+                return cursor < getCount();
+            }
+
+            /**
+             * Returns the next element in the iteration.
+             *
+             * @return the next element in the iteration
+             * @throws NoSuchElementException if the iteration has no more elements
+             */
+            public Float next() {
+                return getData(cursor++);
+            }
+
+            /**
+             * Removes from the underlying collection the last element returned
+             * by this iterator (optional operation).  This method can be called
+             * only once per call to {@link #next}.  The behavior of an iterator
+             * is unspecified if the underlying collection is modified while the
+             * iteration is in progress in any way other than by calling this
+             * method.
+             *
+             * @throws UnsupportedOperationException if the {@code remove}
+             *                                       operation is not supported by this iterator
+             * @throws IllegalStateException         if the {@code next} method has not
+             *                                       yet been called, or the {@code remove} method has already
+             *                                       been called after the last call to the {@code next}
+             *                                       method
+             * {@link UnsupportedOperationException} and performs no other action.
+             */
+            public void remove() {
+
+            }
+        };
+    }
 }
--- src/main/java/com/taxcal/Analysis/SeasonDataAnalysis.java
+++ src/main/java/com/taxcal/Analysis/SeasonDataAnalysis.java
@@ -11,7 +11,7 @@ public class SeasonDataAnalysis {
     /**
      * 用于校准预测的比率模型
      */
-    List<RegressionLine> lines;
+    private List<RegressionLine> lines;
 
     public SeasonDataAnalysis()
     {
@@ -19,7 +19,7 @@ public class SeasonDataAnalysis {
     }
     public void AddData(SeasonData data) throws IllegalAccessException
     {
-        if(datas.get(0).getCount() == data.getCount())
+        if(datas.isEmpty() || datas.get(0).getCount() == data.getCount())
             datas.add(data);
         else
             throw new IllegalArgumentException("销售分季不统一,已导入模型的每年月数和将要导入模型的月数不一致");
@@ -28,14 +28,39 @@ public class SeasonDataAnalysis {
     /**
      * 预测未来年份的各月销售额
      * @param year 指定预测未来第几年的数据，最后一组数据的年份为第0年
-     * @return
+     * @return 返回预测结果集
      */
-    public SeasonData GetData(int year)
-    {
-        float [] r = new float[datas.get(0).getCount()];
+    public SeasonData GetData(int year) throws IllegalAccessException {
+        if(lines == null)
+            throw new IllegalAccessException("模型尚未被建立，无法进行预测");
+        float [] r = new float[datas.get(0).getCount()];    //初始化季度占比列表
+        int index = 0;  //循环基本归纳变量
+        int yearIndex = datas.size() + year;    //年份基准值
+        float sum = 0;          //季度占比矫正变量
 
 
+        PlainDataAnalysis anal = new PlainDataAnalysis();   //建立年度销量预测模型
 
+        for(SeasonData i : datas)                           //分析年度销量
+            anal.AddData(++index, (float) i.getTotal());
+
+        float targetYear = (float) anal.GetData((float) (yearIndex));   //获取年度销量预测值
+        index = 0;
+
+        for(RegressionLine i : lines)                           //分析季度占比预测值
+        {
+            r[index] = i.at(yearIndex);
+            sum += r[index++];
+        }
+
+        for(int i = 0;i < r.length; ++i)                    //矫正预测值
+        {
+            if(r[i] < 0)
+                r[i] = 0;
+            else
+                r[i] /= sum;
+            r[i] = targetYear * r[i];                       //写入实值
+        }
         return new SeasonData(r);
     }
 
@@ -60,7 +85,8 @@ public class SeasonDataAnalysis {
             while(j < datas.get(i).getCount())
             {
                 cap = 100f * datas.get(i).getData(j) / (float)datas.get(i).getTotal();
-                lines.get(j).addDataPoint(new DataPoint(i, cap));
+                lines.get(j).addDataPoint(new DataPoint(i + 1, cap));
+                j++;
             }
         }
     }
