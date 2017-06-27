41184e6148df3b8e6d57816221cb592c1ef71005
Engineer_DDP <zzaddp@live.com>
2017-06-27 14:46:21 +0800

    构建针对季度预测的多元线性回归预测算法

--- src/main/java/com/taxcal/Analysis/SeasonData.java
+++ src/main/java/com/taxcal/Analysis/SeasonData.java
@@ -9,7 +9,7 @@ import java.util.NoSuchElementException;
  * 二维坐标
  */
 
-public class SeasonData implements Iterable<Float>{
+public class SeasonData{
     private List<Float> data;
     private double sum;
 
@@ -38,43 +38,8 @@ public class SeasonData implements Iterable<Float>{
     }
     public int getCount(){return this.data.size(); }
 
-    public Iterator<Float> iterator(){
-        return new Iterator<Float>(){
-            int cursor = 0;
-
-            public boolean hasNext(){
-                return cursor < getCount();
-            }
-
-            /**
-             * Returns the next element in the iteration.
-             *
-             * @return the next element in the iteration
-             * @throws NoSuchElementException if the iteration has no more elements
-             */
-            public Float next() {
-                return getData(cursor++);
-            }
-
-            /**
-             * Removes from the underlying collection the last element returned
-             * by this iterator (optional operation).  This method can be called
-             * only once per call to {@link #next}.  The behavior of an iterator
-             * is unspecified if the underlying collection is modified while the
-             * iteration is in progress in any way other than by calling this
-             * method.
-             *
-             * @throws UnsupportedOperationException if the {@code remove}
-             *                                       operation is not supported by this iterator
-             * @throws IllegalStateException         if the {@code next} method has not
-             *                                       yet been called, or the {@code remove} method has already
-             *                                       been called after the last call to the {@code next}
-             *                                       method
-             * {@link UnsupportedOperationException} and performs no other action.
-             */
-            public void remove() {
-
-            }
-        };
+    public List<Float> GetData()
+    {
+        return data;
     }
 }
